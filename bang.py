a6d86bfeb16d9987ce005857fed8d57962b43111
Cole K <cole-k@users.noreply.github.com>
2017-04-01 18:53:01 -0700

    add bullets

--- bang.py
+++ bang.py
@@ -143,6 +143,22 @@ class Player(Entity):
                 # if you collide, set dy to 0
                 self.dy = 0
 
+    def damage(self,damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.kill()
+
+    def shoot(self):
+        # create a hardcoded bullet
+        x = self.rect.x
+        # lead the bullet differently depending on what direction you're facing
+        if self.direction < 0:
+            x = self.rect.x - 10
+        elif self.direction > 0:
+            x = self.rect.x + self.rect.width
+        bullet = Bullet(x, self.rect.y + round(self.rect.height * .25), 4*self.direction, 0, 8, 3, self.world, 1, self)
+        self.world.bullets.add(bullet)
+
     def jump(self):
         """
         Increases y velocity
@@ -185,8 +201,47 @@ class Platform(Entity):
     # if you want moving platforms, specify here minimum and maximum x and y values
     def __init__(self,x,y,dx,dy,width,height,world,passthrough=True,dropdown=True):
         super().__init__(x,y,dx,dy,width,height,world,color=(255,0,255))
+        self.type = 'platform'
         self.passthrough = True
         self.dropdown = True
+
+class Bullet(Entity):
+    def __init__(self,x,y,dx,dy,width,height,world,damage,player):
+        super().__init__(x,y,dx,dy,width,height,world,color=(0,0,0))
+        self.type = 'bullet'
+        self.damage = damage
+        # player who fired the bullet
+        self.player = player
+
+    def update(self):
+        self.rect.x += self.dx
+        # first check for collisions in platforms
+        platforms_hit = pygame.sprite.spritecollide(self, self.world.platforms, False)
+        if platforms_hit:
+            # kill the sprite if we collide with a platform
+            self.kill()
+        # check for player collisions
+        players_hit = pygame.sprite.spritecollide(self, self.world.players, False)
+        if players_hit:
+            self.kill()
+            # damage all players if you intersect instead of undefined behavior
+            for player in players_hit:
+                if player != self.player:
+                    player.damage(self.damage)
+        # check for bullet collisions
+        bullets_hit = pygame.sprite.spritecollide(self, self.world.bullets, False)
+        if bullets_hit:
+            for bullet in bullets_hit:
+                # if the bullet is owned by another player
+                if self.player != bullet.player:
+                    # destroy both
+                    self.kill()
+                    bullet.kill()
+        # destroy the bullet if it gets out of bounds
+        if self.rect.x > self.world.width or self.rect.y < 0:
+            self.kill()
+
+
 
 def main():
     width, height = 700, 500
@@ -217,12 +272,16 @@ def main():
                     player.move_right()
                 if event.key == pygame.K_UP:
                     player.jump()
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
                 if event.key == pygame.K_a:
                     player2.move_left()
                 if event.key == pygame.K_d:
                     player2.move_right()
                 if event.key == pygame.K_w:
                     player2.jump()
+                if event.key == pygame.K_s:
+                    player2.shoot()
 
         world.update()
         world.draw()
